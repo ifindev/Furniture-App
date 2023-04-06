49450b52480294047b932207a65f2f6f280699b7
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-04-06 20:37:02 +0700

    feat:sign in screen UI

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import SignUpView from './src/screens/auth/SignUp/SignUp.view';
+import SignInView from './src/screens/auth/SignIn/SignIn.view';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <SignUpView />
+      <SignInView />
     </SafeAreaView>
   );
 }
