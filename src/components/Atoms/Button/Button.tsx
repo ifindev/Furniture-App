706ded0ed485e5c09e8e886aef2b5b4f02cd3ed5
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-04-06 11:01:45 +0700

    feat: update Button with TouchableOpacity

--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import {styled} from 'nativewind';
 import React, {useMemo} from 'react';
-import {Pressable, PressableProps} from 'react-native';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 import Text from '../Text/Text';
 
 const buttonTypes = {
@@ -14,9 +14,9 @@ const textType = {
   text: styled(Text, clsx('text-center text-purple font-bold')),
 };
 
-const Container = styled(Pressable, clsx('py-5 rounded-lg'));
+const Container = styled(TouchableOpacity, clsx('py-5 rounded-lg'));
 
-interface Props extends Pick<PressableProps, 'onPress'> {
+interface Props extends Pick<TouchableOpacityProps, 'onPress'> {
   buttonType?: 'solid' | 'text';
   text: string;
 }
@@ -25,7 +25,10 @@ export default function Button({buttonType = 'solid', text, onPress}: Props) {
   const TextBody = useMemo(() => textType[buttonType], [buttonType]);
 
   return (
-    <Container className={buttonTypes[buttonType]} onPress={onPress}>
+    <Container
+      className={buttonTypes[buttonType]}
+      onPress={onPress}
+      activeOpacity={0.8}>
       <TextBody>{text}</TextBody>
     </Container>
   );
